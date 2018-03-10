41bbbd778a2e8c2cb7f2b72dd67e5ab69369c335
Jenna Allen <jallen1006@gmail.com>
2018-03-09 22:07:35 -0800

    css updates and app updates

--- dog_records_app.R
+++ dog_records_app.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(timevis)
 library(DT)
 
-source("utils.R")
+# source("utils.R")
 
 dimTests <- read_csv("dimTests.csv")
 dimMeds <- read_csv("dimMeds.csv")
@@ -31,9 +31,9 @@ all_visit_categories <- dimVisits %>%
 
 # UI
 ui <- fluidPage(
-   # tags$head(
-   #   tags$link(href = "style.css", rel = "stylesheet")
-   # ),
+    tags$head(
+      tags$link(href = "style.css", rel = "stylesheet")
+    ),
   #sidebarLayout(
   #tags$style(HTML('.vis-item.vis-selected td, table.dataTable td.selected {background-color: green !important;}')),
   #tags$style(HTML('table.dataTable tr.selected td, table.dataTable td.selected {background-color: pink !important;}')),
@@ -48,9 +48,9 @@ ui <- fluidPage(
                                      choices = all_visit_categories,
                                      selected = "medical")), width = 2),
       column(wellPanel(timevisOutput("timeline")
-                       ), width = 7
+                       ), width = 6
              ),
-      column(wellPanel(DT::dataTableOutput(outputId = "med_tests_table")), width = 3)
+      column(wellPanel(DT::dataTableOutput(outputId = "med_tests_table")), width = 4)
       # column(wellPanel(DT::dataTableOutput(outputId = "teststable")
       #                  ),
       #        width = 3,
@@ -84,60 +84,39 @@ ui <- fluidPage(
 # Server
 server <- function(input, output) {
   
-  # Create med history timeline
+  # Create timeline
   output$timeline <- renderTimevis({
     req(input$dog, input$visit_category)
     config <- list(
       orientation = "top",
       multiselect = TRUE
-    )
+      )
+    
     dimVisits %>% 
       inner_join(dimDogs, by = "dog_name") %>% 
       filter(dog_name %in% input$dog, visit_category %in% input$visit_category) %>%
       rename(id = visit_id, content = visit_summary, start = visit_date) %>% 
-        # distinct() %>% 
-      # mutate(style = "color: red; background-color: pink;",
-      #        title = "test") %>% 
-        timevis(options = config)
-  })
+       mutate(className = case_when(
+         visit_category == "medical" ~ "medical",
+         visit_category == "medical follow-up" ~ "medical-follow-up",
+         visit_category == "routine" ~ "routine"),
+         # possibly unite some fields together to make what displays in the title
+         title = visit_notes) %>% 
+      timevis(options = config)
+    })
   
- 
+  # Create tests data table
   output$med_tests_table <- DT::renderDataTable({
     req(input$dog)
     dimVisits %>% 
       inner_join(dimDogs, by = "dog_name") %>% 
       inner_join(dimTests, by = c("dog_name", "visit_date" = "test_date_given", "facility_name")) %>%
-      filter(dog_name %in% input$dog, visit_category %in% input$visit_category,!(test_name %in% c("Fecal Test", "Heartworm Test"))) %>% 
+      filter(dog_name %in% input$dog, visit_category %in% input$visit_category) %>% 
       select(visit_id, visit_date, test_name, test_result) %>% 
       datatable(options = list(pageLength = 10),
                                rownames = FALSE)
-  #      # mutate(selected = if_else(id %in% input$med_history_timeline_selected, 1, 0)) %>% 
-  #      # datatable(options = list(pageLength = 10, 
-  #      #                          columnDefs = list(list(targets = 6, visible = FALSE))
-  #      #                          )
-  #      #           ) %>% 
-  #     #  formatStyle("selected", target = "row", backgroundColor = styleEqual(c(0, 1), c('transparent', 'yellow'))
-  #     #              ) #%>% 
-  #     #select(id, content)
    })
 
-  # # hide V6
-  # datatable(df, options = list(
-  #   columnDefs = list(list(targets = 6, visible = FALSE))
-  # )) %>% formatStyle(
-  #   'V1', 'V6',
-  #   backgroundColor = styleEqual(c(0, 1), c('gray', 'yellow'))
-  # )
-
-  
-  # # Create tests data table
-  # output$teststable <- renderTable({
-  #   req(input$dog)
-  #   tests_from_selected_dog <- dimTests %>%
-  #     filter(dog_name %in% input$dog) %>%
-  #     select(test_name, test_result)
-  # })
-  # 
   # # Create current meds data table
   # output$current_meds_table <- DT::renderDataTable({
   #   req(input$dog)
