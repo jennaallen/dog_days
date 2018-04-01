ade5f5224795144c152d5e5231c085994c4f9418
Jenna Allen <jallen1006@gmail.com>
2018-04-01 11:42:11 -0700

    moved deleted files

--- utils.R
+++
@@ -1,9 +0,0 @@
-prettyDate <- function(d) {
-  suppressWarnings(format(as.Date(gsub("T", " ", d), "%Y-%m-%d")))
-}
-
-writeBin_filepath <- function(x, path) {
-  writeBin(x, path)
-  addResourcePath("private", normalizePath(tempdir()))
-  return(invisible(path))
-}
